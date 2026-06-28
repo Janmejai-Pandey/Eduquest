601f4c969ed3af75fa1f76eb0d2af09212708eb5
Janmejai Pandey <janmejai.pandey0403@gmail.com>
2026-06-28 13:13:42 +0530

    Configration files updated.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 cd ./src
 uvicorn api:app --reload --port 8000
 ```
+config files contains import import informations.
---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+import sys
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.join(PROJECT_ROOT, "src")
+IR_DIR = os.path.join(SRC_DIR, "IR")
+CHAT_DIR = os.path.join(SRC_DIR, "chat")
+SUMMARISER_DIR = os.path.join(SRC_DIR, "summariser")
+QUIZ_DIR = os.path.join(SRC_DIR, "quiz")
+RESUME_DIR = os.path.join(SRC_DIR, "resume_project")
+
+GEMINI_API_KEY = "AQ.Ab8RN6J8Kh8wq3MJ1kyvxJR0qRPUsLvA5hOHYIQT3fag_Ey_oQ"
+
+#import all the directories to sys.path
+sys.path.insert(0, SRC_DIR)
+sys.path.insert(0, IR_DIR)
+sys.path.insert(0, CHAT_DIR)
+sys.path.insert(0, SUMMARISER_DIR)
+sys.path.insert(0, QUIZ_DIR)
+
+def all_imports():
+    sys.path.insert(0, SRC_DIR)
+    sys.path.insert(0, IR_DIR)
+    sys.path.insert(0, CHAT_DIR)
+    sys.path.insert(0, SUMMARISER_DIR)
+    sys.path.insert(0, QUIZ_DIR)
+    
+def chat_imports():
+    sys.path.insert(0, IR_DIR)
+    sys.path.insert(0, CHAT_DIR)
+    sys.path.insert(0, SUMMARISER_DIR)
+    
+def resume_imports():
+    sys.path.insert(0, IR_DIR)
+    sys.path.insert(0, RESUME_DIR)
